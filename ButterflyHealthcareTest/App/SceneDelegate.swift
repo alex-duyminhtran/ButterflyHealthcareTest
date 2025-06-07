de11978b6597b509656b3c9a9fb17d03a8c53033
Alex <alex.duyminhtran@gmail.com>
2025-06-07 16:53:14 +1000

    Create the project

--- ButterflyHealthcareTest/App/SceneDelegate.swift
+++ ButterflyHealthcareTest/App/SceneDelegate.swift
@@ -11,14 +11,31 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let tabBarController = UITabBarController()
+        tabBarController.viewControllers = [
+            createNavController(for: SearchViewController(), title: "Movies", image:UIImage(systemName: "film")),
+            createNavController(for: FavoriteViewController(), title: "Favorites", image: UIImage(systemName: "star.fill"))
+           ]
+
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = tabBarController
+        window?.makeKeyAndVisible()
     }
 
+    private func createNavController(for rootViewController: UIViewController,
+                                     title: String,
+                                     image: UIImage?) -> UINavigationController {
+           let navController = UINavigationController(rootViewController: rootViewController)
+           navController.tabBarItem.title = title
+           navController.tabBarItem.image = image
+           rootViewController.navigationItem.title = title
+        
+           return navController
+       }
+    
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
